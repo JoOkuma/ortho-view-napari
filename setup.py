254986af51d78811d862929f0576fd874c9d2f21
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2021-06-15 13:38:06 -0700

    added napari entrypoint

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@ from setuptools import setup
 
 # https://github.com/pypa/setuptools_scm
 use_scm = {"write_to": "ortho_view_napari/_version.py"}
-setup(use_scm_version=use_scm)
+setup(
+      use_scm_version=use_scm,
+      entry_points={'napari.plugin': 'OtherView = ortho_view_napari'},
+)
