4014f5b912de37798d55ed1883c3ffb6eb9939e2
Draga Doncila Pop <ddoncila@gmail.com>
2023-09-28 11:17:15 +1000

    Add npe2 manifest and visibility flag

--- setup.py
+++ setup.py
@@ -7,5 +7,4 @@ from setuptools import setup
 use_scm = {"write_to": "ortho_view_napari/_version.py"}
 setup(
       use_scm_version=use_scm,
-      entry_points={'napari.plugin': 'OtherView = ortho_view_napari'},
 )
